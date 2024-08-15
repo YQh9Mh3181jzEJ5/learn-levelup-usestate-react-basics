9bbf2908265b05ec7c9d0773c45d5e1780059e8a
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-15 17:48:35 +0900

    fix: add Arrow

--- src/components/RuleList.tsx
+++ src/components/RuleList.tsx
@@ -1,15 +1,28 @@
 import React, { useState } from "react";
 import { EXP_INCREMENT, EXP_TO_LEVEL_UP, INITIAL_LEVEL } from "@/config";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, ArrowRight } from "lucide-react";
 
 export const RulesList: React.FC = (): JSX.Element => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
   const rules = [
-    `経験値は、「経験値アップ」ボタンをクリックするごとに +${EXP_INCREMENT}`,
-    `経験値${EXP_TO_LEVEL_UP} 以上で、レベルが +1`,
-    "レベルは、「レベルアップ」ボタンをクリックするごとに +1（経験値は初期化）",
-    "「リセット」ボタンをクリックすると、経験値とレベルが初期化",
+    {
+      title: "経験値アップ",
+      description: `「経験値アップ」ボタンをクリックするごとに +${EXP_INCREMENT}`,
+    },
+    {
+      title: "レベルアップ条件",
+      description: `経験値${EXP_TO_LEVEL_UP} 以上で、レベルが +1`,
+    },
+    {
+      title: "手動レベルアップ",
+      description:
+        "「レベルアップ」ボタンをクリックするごとに +1（経験値は初期化）",
+    },
+    {
+      title: "リセット",
+      description: "「リセット」ボタンをクリックすると、経験値とレベルが初期化",
+    },
   ];
 
   return (
@@ -18,7 +31,7 @@ export const RulesList: React.FC = (): JSX.Element => {
         className="flex justify-between items-center w-full p-4"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <h2>ルール</h2>
+        <h2 className="text-ms font-semibold">ルール</h2>
         {isOpen ? (
           <ChevronUp className="w-5 h-5" />
         ) : (
@@ -26,13 +39,17 @@ export const RulesList: React.FC = (): JSX.Element => {
         )}
       </button>
       {isOpen && (
-        <div className="flex flex-col gap-2 p-4">
-          <ol className="list-decimal list-inside">
-            {rules.map((rule, index) => (
-              <li key={index}>{rule}</li>
-            ))}
-          </ol>
-          <p className="">
+        <div className="flex flex-col gap-4 p-4">
+          {rules.map((rule, index) => (
+            <div key={index} className="flex items-start">
+              <ArrowRight className="w-4 h-4 mt-1 mr-2 flex-shrink-0 text-blue-400" />
+              <div>
+                <h3 className="font-medium text-blue-400">{rule.title}</h3>
+                <p className="mt-1 text-gray-300">{rule.description}</p>
+              </div>
+            </div>
+          ))}
+          <p className="mt-2 italic text-gray-400">
             ※初期値は、経験値: 0, レベル: {INITIAL_LEVEL}とする。
           </p>
         </div>
