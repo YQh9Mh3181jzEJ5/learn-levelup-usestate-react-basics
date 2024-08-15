3d6992b9cf73d6c9c97fbe1ddb8a7d4e671e2081
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-15 17:45:44 +0900

    feat: add acordion

--- src/components/RuleList.tsx
+++ src/components/RuleList.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { EXP_INCREMENT, EXP_TO_LEVEL_UP, INITIAL_LEVEL } from "@/config";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 export const RulesList: React.FC = (): JSX.Element => {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+
   const rules = [
     `経験値は、「経験値アップ」ボタンをクリックするごとに +${EXP_INCREMENT}`,
     `経験値${EXP_TO_LEVEL_UP} 以上で、レベルが +1`,
@@ -10,16 +13,30 @@ export const RulesList: React.FC = (): JSX.Element => {
   ];
 
   return (
-    <div className={`text-sm p-4 bg-slate-800 rounded`}>
-      <h2>ルール</h2>
-      <ol className="list-decimal list-inside mt-2">
-        {rules.map((rule, index) => (
-          <li key={index}>{rule}</li>
-        ))}
-      </ol>
-      <p className="mt-2">
-        ※初期値は、経験値: 0, レベル: {INITIAL_LEVEL}とする。
-      </p>
+    <div className="text-sm bg-slate-800 rounded">
+      <button
+        className="flex justify-between items-center w-full p-4"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <h2>ルール</h2>
+        {isOpen ? (
+          <ChevronUp className="w-5 h-5" />
+        ) : (
+          <ChevronDown className="w-5 h-5" />
+        )}
+      </button>
+      {isOpen && (
+        <div className="flex flex-col gap-2 p-4">
+          <ol className="list-decimal list-inside">
+            {rules.map((rule, index) => (
+              <li key={index}>{rule}</li>
+            ))}
+          </ol>
+          <p className="">
+            ※初期値は、経験値: 0, レベル: {INITIAL_LEVEL}とする。
+          </p>
+        </div>
+      )}
     </div>
   );
 };
