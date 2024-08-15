a5cc8adfecfc9cb3da3d101367662c61a9a57bb5
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-16 07:15:59 +0900

    fix: ui

--- src/components/GameRules.tsx
+++ src/components/GameRules.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { RulesList } from "./RuleList";
 
 export const GameRules: React.FC = (): JSX.Element => (
-  <div className="flex ">
+  <div className="flex justify-between items-center">
     <header className="text-2xl text-center my-4">
       ポケモン風？カウントアップ
     </header>
