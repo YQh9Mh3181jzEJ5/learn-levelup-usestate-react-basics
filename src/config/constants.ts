524f6f6984f7ea3b53f4ff2cf1aae33660c32068
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-08-15 22:08:19 +0900

    add comments for constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,5 @@
-export const EXP_INCREMENT = 20;
-export const EXP_TO_LEVEL_UP = 100;
-export const INITIAL_LEVEL = 1;
-export const INITIAL_EXP = 0;
-export const MAX_LEVEL = 100;
+export const EXP_INCREMENT = 20; // 1回の経験値の増加量
+export const EXP_TO_LEVEL_UP = 100; // レベルアップに必要な経験値
+export const INITIAL_LEVEL = 1; // 初期レベル
+export const INITIAL_EXP = 0; // 初期経験値
+export const MAX_LEVEL = 100; // 最大レベル
